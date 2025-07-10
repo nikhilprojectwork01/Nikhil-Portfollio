ca6f2dbe24fa90c1e2ff2bba8e2b9bcb6d1d8551
developer Nikhil <nikhil00pandey@gmai.com>
2025-07-10 17:37:07 +0530

    adding all files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -82,7 +82,7 @@ const About = () => {
             <img
               src={profileImage}
               alt="Nikhil Pandey"
-              className="w-full h-full rounded-lg object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
         </div>
